e9e5834d2ca12db81bdd898d2d512f92b4fa0c32
Jake Dube <jake@integrity-sg.com>
2018-06-22 22:16:03 -0400

    Bump version number

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@
 # ##### END GPL LICENSE BLOCK #####
 
 """
-===== MAZE GENERATOR [PRO] V.2.3.3 =====
+===== MAZE GENERATOR [PRO] V.2.3.4 =====
 This __init__ module handles some UI and also registers all
 classes and properties.
 """
@@ -51,7 +51,7 @@ logger = setup_logger(__name__)
 bl_info = {
     "name": "UltiMaze [PRO]",
     "author": "Jake Dube",
-    "version": (2, 3, 3),
+    "version": (2, 3, 4),
     "blender": (2, 76, 0),
     "location": "3D View > Tools > Maze Gen",
     "description": "Generates 3-dimensional mazes.",
